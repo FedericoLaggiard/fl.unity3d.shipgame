272b686b762d8ed26bbe83d0caa3868025809fa6
federicolaggiard <federico.laggiard@spindox.it>
2016-11-29 15:08:24 +0100

    add chain test

--- Assets/AccellerometerInput.cs
+++ Assets/AccellerometerInput.cs
@@ -14,13 +14,25 @@ public class AccellerometerInput : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        dir = Vector3.zero;
+		dir = Vector3.zero;
 
-        dir.x = Input.acceleration.x;
-        //dir.y = Input.acceleration.y;
-        //dir.z = Input.acceleration.z;
+		#if UNITY_STANDALONE || UNITY_EDITOR
 
-        if (dir.sqrMagnitude > 1)
-            dir.Normalize();
+			float temp = Input.GetAxis("Horizontal");
+			
+			if (temp != 0){
+				dir.x =  temp;
+			}
+			
+		#else
+
+	        dir.x = Input.acceleration.x;
+	        //dir.y = Input.acceleration.y;
+	        //dir.z = Input.acceleration.z;
+
+		#endif
+
+		if (dir.sqrMagnitude > 1)
+			dir.Normalize();
 	}
 }
