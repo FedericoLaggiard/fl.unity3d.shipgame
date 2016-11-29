e341b4b4aa2b373831d1ba4f4faa02eff6057154
federicolaggiard <federico.laggiard@spindox.it>
2016-11-29 11:19:13 +0100

    initial

---
+++ Assets/AccellerometerInput.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AccellerometerInput : MonoBehaviour {
+
+    [HideInInspector]
+    public Vector3 dir = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        dir = Vector3.zero;
+
+        dir.x = Input.acceleration.x;
+        //dir.y = Input.acceleration.y;
+        //dir.z = Input.acceleration.z;
+
+        if (dir.sqrMagnitude > 1)
+            dir.Normalize();
+	}
+}
---
+++ Assets/controller.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class controller : MonoBehaviour {
+
+    public AccellerometerInput input;
+    public Rigidbody2D Shiprigidbody;
+    public float moveForce = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+        if (!input)
+            throw new System.Exception("No input added");
+        if (!Shiprigidbody)
+            throw new System.Exception("No rigidbody");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Shiprigidbody.AddForce(input.dir * moveForce);
+	}
+}
